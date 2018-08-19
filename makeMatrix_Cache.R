c1c3cb96fbbfb73a436b7e3193b683e1fb39e1be
divyaakella0108 <akelladivya.0108@gmail.com>
2018-08-19 18:46:06 +0530

    commit
    commit

---
+++ makeMatrix_Cache.R
@@ -0,0 +1,26 @@
+makeMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+cacheinv <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
